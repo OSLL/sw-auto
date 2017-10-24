3b8cb2218505be1ef9894352ef1114ed07296267
EduardBlees <Edw252@gmail.com>
2017-10-24 22:41:39 +0300

    Basic script logic

--- md_check_scripts/key_questions.py
+++ md_check_scripts/key_questions.py
@@ -4,6 +4,86 @@ import mistune
 from pathlib import Path
 from bs4 import BeautifulSoup
 
+PURPOSE_STATEMENT_MIN = 300
+
+def CheckRepo(repoPath):
+    print('Checking for directory "' + repoPath + '"')
+    directory = Path(repoPath)
+    if directory.is_dir():
+        print(' Directory'.ljust(26) +  '...\tExists!') # \u2713
+        CheckGroupDirectory(repoPath + '\\3303')
+        CheckGroupDirectory(repoPath + '\\3304')
+        CheckGroupDirectory(repoPath + '\\3381')
+    else:
+        print("Directory doesn't exist!")
+
+def CheckGroupDirectory(groupFolderPath):
+    print('Checking for directory "' + groupFolderPath + '"')
+    directory = Path(groupFolderPath)
+    if directory.is_dir():
+        print(' Directory'.ljust(26) +  '...\tExists!') # \u2713
+        #find all nameFolders
+    else:
+        print("Directory doesn't exist!")
+
+def CheckNamedDirectory(nameFolderPath):
+    print('Checking for directory "' + nameFolderPath + '"')
+    directory = Path(nameFolderPath)
+
+    if directory.is_dir():
+        print(' Directory'.ljust(26) +  '...\tExists!') # \u2713
+        CheckForPaperBase(nameFolderPath + '\\paper_base')
+        CheckForFactResult(nameFolderPath)
+        CheckPurposeStatementFiles(nameFolderPath)
+        CheckForAnalogs(nameFolderPath)     
+    else:
+        print("Directory doesn't exist!")
+
+def CheckForPaperBase(paperBaseFolderPath):
+    #.pdf file(s)
+    print("CheckForPaperBase")
+
+def CheckForFactResult(factResFolderPath):
+    #1200 symbols
+    print("CheckForFactResult")
+
+def CheckForAnalogs(analogsFolderPath):
+    #2000symbols, min 3 analogs, min 3 criterias, references
+    print("CheckForAnalogs")
+    pass
+
+def CheckPurposeStatementFiles(nameFolderPath):
+    print('Checking for purpose-statement files...')
+    if (
+        CheckForFile(nameFolderPath,"problem.md") &
+        CheckForFile(nameFolderPath,"research_object.md") &
+        CheckForFile(nameFolderPath,"research_subject.md") &
+        CheckForFile(nameFolderPath,"goal.md") &
+        CheckForFile(nameFolderPath,"tasks.md") &
+        CheckForFile(nameFolderPath,"relevance.md") ):
+        print('All required files exist!\nCounting symbols...')            
+        purposeSymbNum = CountPurposeTextSymbols()
+        if purposeSymbNum >= PURPOSE_STATEMENT_MIN:
+            print("Number of symbols in purpose statement's".ljust(50) + '...\tGood.')
+        else:
+            print("Number of symbols in purpose statement's".ljust(50) + '...\tNot enough. Min = ' + \
+                str(PURPOSE_STATEMENT_MIN))
+    else:
+        print('\nPlease, add all required files to the directory!')
+
+def CountPurposeTextSymbols():
+    allSymbolsNum = CountTextSymbols("problem.md") + \
+            CountTextSymbols("research_object.md") + \
+            CountTextSymbols("research_subject.md") + \
+            CountTextSymbols("goal.md") + \
+            CountTextSymbols("tasks.md") + \
+            CountTextSymbols("relevance.md")
+    if allSymbolsNum == 0:
+        print('All files are empty!')
+    else:
+        print('All files'.ljust(26) + '...\t' + str(allSymbolsNum) + ' symbols')
+    return allSymbolsNum
+
 def CheckForFile(dirpath, filename):
     file = Path(dirpath+'/' + filename)
     
@@ -22,7 +102,7 @@ def ParseMd(file):
     return soup
 
 def CountTextSymbols(filename):
-    soup = ParseMd(path + '/' + filename)
+    soup = ParseMd(repo_path + '\\' + filename)
     pars = soup.find_all('p')
     allText = ""
     for t in pars:
@@ -35,41 +115,9 @@ def CountTextSymbols(filename):
         print(' ' + filename.ljust(25) + '...\t' + str(symbolsNum) + ' symbols')
     return symbolsNum
 
-def CountAllTextSymbols():
-    allSymbolsNum = CountTextSymbols("problem.md") + \
-            CountTextSymbols("research_object.md") + \
-            CountTextSymbols("research_subject.md") + \
-            CountTextSymbols("goal.md") + \
-            CountTextSymbols("tasks.md") + \
-            CountTextSymbols("relevance.md")
-    if allSymbolsNum == 0:
-        print('All files are empty!')
-    else:
-        print('All files'.ljust(26) + '...\t' + str(allSymbolsNum) + ' symbols')
-
 parser = argparse.ArgumentParser()
 parser.add_argument('path', help='path to directory with .md files')
 args = parser.parse_args()
-path = args.path
+repo_path = args.path
 
-print('Checking for directory...')
-
-directory = Path(path)
-if directory.is_dir():
-    print(' Directory'.ljust(26) +  '...\tExists!') # \u2713
-    print('Checking for files...')
-    if (
-        CheckForFile(path,"problem.md") &
-        CheckForFile(path,"research_object.md") &
-        CheckForFile(path,"research_subject.md") &
-        CheckForFile(path,"goal.md") &
-        CheckForFile(path,"tasks.md") &
-        CheckForFile(path,"relevance.md") ):
-        print('All required files exist!\nCounting symbols...')
-        
-        CountAllTextSymbols()
-    else:
-        print('\nPlease, add all required files to the directory!')
-else:
-    print("Directory doesn't exist!")
-    
+CheckRepo(repo_path)

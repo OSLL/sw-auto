7c80e1e074a84bab453afbe32a5fb0483c8a80a3
EduardBlees <edw252@gmail.com>
2017-11-30 19:22:15 +0300

    Zipfs law graph

--- ssa_algorithms/zipf.py
+++ ssa_algorithms/zipf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-# import nltk
+
 import os
 import codecs
 import mistune
@@ -10,13 +10,11 @@ from glob import glob
 from pathlib import Path
 from bs4 import BeautifulSoup
 import re
-# import scipy.stats as ss
-# import math
-# import numpy
-# import matplotlib
-
-# from nltk.corpus import reuters
-# from nltk.corpus import wordnet
+import pymorphy2
+import scipy.stats as ss
+import math
+import numpy
+import matplotlib.pyplot as plt
 
 def CheckForFile(dirpath, filename):
     file = Path(os.path.join(dirpath,filename))
@@ -47,30 +45,42 @@ def GetText(filename):
         allText += t.text + ' '
     return allText
 
+def CountWords(wordList):
+    morph = pymorphy2.MorphAnalyzer()
+    counts = {}
+    for _word in wordList:
+        currForm = morph.parse(_word)[0]
+        nounForm = currForm.inflect({'sing','nomn'})
+
+        try:
+            word = nounForm.word
+        except:
+            word = currForm.word
+        
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+
 def GetStats(filename):
     CheckForFile(os.getcwd(), filename)
     allText = GetText(os.path.join(os.getcwd(),filename))
     wordList = re.sub("[^\w]", " ",  allText).split()
     wordList = [w.lower() for w in wordList]
-    words = set(wordList)
-    counts = [(w, wordList.count(w)) for w in words]
+    counts = CountWords(wordList)
 
-    for (w, c) in counts:
-        if c > 1:
-            print(w + ": " + str(c))  
+    for word, freq in counts.items():
+        if freq > 2:
+            print(word + ": " + str(freq))
     
-    # amb = [(w, c, len(wordnet.synsets(w))) for (w, c) in counts if len(wordnet.synsets(w)) > 0]
-    
-    # amb_p_rank = ss.rankdata([p for (w, c, p) in amb])
-    # amb_c_rank = ss.rankdata([c for (w, c, p) in amb])
-    
-    # amb_ranked = zip(amb, amb_p_rank, amb_c_rank)    
+    amb = [(w, c) for (w, c) in counts.items()]    
+    amb_c_rank = ss.rankdata([c for (w, c) in amb])
 
-    # numpy.corrcoef(amb_c_rank, [math.log(c) for (w, c, p) in amb])
-    
-    # rev = [l-r+1 for r in amb_c_rank]
-    
-    # plt.plot([math.log(c) for c in rev], [math.log(c) for (w, c, p) in amb], 'ro')
+    #numpy.corrcoef(amb_c_rank, [math.log(c) for (w, c) in amb])
+    rev = [len(amb_c_rank)-r+1 for r in amb_c_rank]    
+    plt.plot([math.log(c) for c in rev], [math.log(c) for (w, c) in amb], 'ro')
+    plt.show()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path', help='path to directory with .md files')

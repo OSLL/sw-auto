9964424af2708f3d2f5117a85be293ff138b521b
EduardBlees <Edw252@gmail.com>
2017-12-07 00:50:24 +0300

    Zipfs: Get text from directory

--- ssa_algorithms/zipf.py
+++ ssa_algorithms/zipf.py
@@ -16,6 +16,16 @@ import math
 import numpy
 import matplotlib.pyplot as plt
 
+def CheckForDir(dirpath):
+    dir = Path(dirpath)
+
+    if dir.is_dir():
+        print('     ' + str(dir).ljust(25) + '...\tExists!')
+        return True
+    else:
+        print('     ' + str(dir).ljust(25) + "...\tDoesn't exist!")
+        return False
+
 def CheckForFile(dirpath, filename):
     file = Path(os.path.join(dirpath,filename))
 
@@ -37,10 +47,21 @@ def ParseMd(file):
     soup = BeautifulSoup(htmlText, 'html.parser')
     return soup
 
+def GetAllText(dirpath):
+    files = os.listdir(dirpath)
+    mdFiles = [f for f in files if f.endswith(".md")]
+    mdFiles = [os.path.join(dirpath, f) for f in mdFiles]
+    allText = ''
+    for file in mdFiles:
+        allText += GetText(file) + ' '
+    return allText
+
 def GetText(filename):
     soup = ParseMd(filename)
+    #print(filename)
+    #print(soup) #encode("utf-8-sig")
     pars = soup.find_all(['p','li'])
-    allText = ""
+    allText = " "
     for t in pars:
         allText += t.text + ' '
     return allText
@@ -63,9 +84,9 @@ def CountWords(wordList):
             counts[word] = 1
     return counts
 
-def GetStats(filename):
-    CheckForFile(os.getcwd(), filename)
-    allText = GetText(os.path.join(os.getcwd(),filename))
+def GetStats(dirPath):
+    CheckForDir(dirPath)
+    allText = GetAllText(dirPath)
     wordList = re.sub("[^\w]", " ",  allText).split()
     wordList = [w.lower() for w in wordList]
     counts = CountWords(wordList)
@@ -73,18 +94,25 @@ def GetStats(filename):
     for word, freq in counts.items():
         if freq > 2:
             print(word + ": " + str(freq))
-    
+   
     amb = [(w, c) for (w, c) in counts.items()]    
     amb_c_rank = ss.rankdata([c for (w, c) in amb])
+    amb_sorted = sorted(amb, key=lambda x: x[1], reverse=True)
 
     #numpy.corrcoef(amb_c_rank, [math.log(c) for (w, c) in amb])
     rev = [len(amb_c_rank)-r+1 for r in amb_c_rank]    
-    plt.plot([math.log(c) for c in rev], [math.log(c) for (w, c) in amb], 'ro')
+    #plt.plot([math.log(c) for c in rev], [math.log(c) for (w, c) in amb], 'ro')
+
+    x = range(0, len(amb_sorted[0:5]))
+    y = [c for (w, c) in amb_sorted[0:5]]
+    my_xticks = [w for (w, c) in amb_sorted[0:5]]
+    plt.xticks(x, my_xticks)
+    plt.plot(x, y)
     plt.show()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path', help='path to directory with .md files')
 args = parser.parse_args()
-file_path = args.path
+dir_path = args.path
 
-GetStats(file_path)
+GetStats(dir_path)

a7c75913d99b37c58e45087fb59c945722c16449
EduardBlees <edw252@gmail.com>
2018-03-15 00:17:07 +0300

    dependencies

---
+++ ssa_algorithms/dependencies.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr \
+flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig
+pip install textract

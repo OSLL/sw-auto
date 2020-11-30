8b007d2608ef4d3bf165dfdbffa6e3b4ae875271
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2020-11-30 13:38:33 +0300

    intermidiate commit

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+admin_password=${1}
+mongo_password=${2}
+
+
+echo "Inserting passwords"
+#Inserting mongo password
+echo "MONGO_INITDB_ROOT_PASSWORD=${mongo_password}" > .env
+echo "ADMIN_PASSWORD=${admin_password}" >> .env
+
+cd SciencePaperAnalyzer/
+
+echo "Stopping old configuration"
+docker-compose -f ./docker-compose_prod.yml down || true
+
+echo "Building "
+docker-compose -f ./docker-compose_prod.yml build
+
+echo "Running"
+docker-compose -f ./docker-compose_prod.yml up -d
+
+echo "Getting startup logs"
+
+sleep 10s
+
+docker-compose -f ./docker-compose_prod.yml logs

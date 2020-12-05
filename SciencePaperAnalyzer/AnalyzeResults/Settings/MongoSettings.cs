c0244a8a20bfb85965816020339af6a2eee262fb
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2020-12-05 19:23:56 +0300

    recaptcha and 2 other fixes

--- SciencePaperAnalyzer/AnalyzeResults/Settings/MongoSettings.cs
+++ SciencePaperAnalyzer/AnalyzeResults/Settings/MongoSettings.cs
@@ -12,8 +12,8 @@
             get
             {
                 if (string.IsNullOrEmpty(User) || string.IsNullOrEmpty(Password))
-                    return $@"mongodb://{Host}:{Port}/{Database}";
-                return $@"mongodb://{User}:{Password}@{Host}:{Port}/{Database}";
+                    return $@"mongodb://{Host}:{Port}";
+                return $@"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
